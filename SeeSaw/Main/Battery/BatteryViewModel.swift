6c1b5a8b7cf8fe0346c52438cbd7e9462fa2579d
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-06-17 14:17:43 +0900

    Refactor(battery): GetBattery DTO 파일 분리

--- SeeSaw/Main/Battery/BatteryViewModel.swift
+++ SeeSaw/Main/Battery/BatteryViewModel.swift
@@ -10,33 +10,6 @@ import Combine
 import Foundation
 import KeychainSwift
 
-struct BatteryInfo: Codable {
-    let battery: Int
-    let fastChargeTitle: String?
-    let fastChargeValue: String?
-    let todayActivity: Int?
-    let activityGoal: Int?
-    let todaySleep: Int?
-    let sleepGoal: Int?
-    
-    private enum CodingKeys: String, CodingKey {
-        case battery = "curBattery"
-        case fastChargeTitle = "chargeName"
-        case fastChargeValue = "valueName"
-        case todayActivity = "curActivity"
-        case activityGoal = "activityGoal"
-        case todaySleep = "curSleep"
-        case sleepGoal = "sleepGoal"
-    }
-}
-
-struct GetBatteryResponse: Codable {
-    let isSuccess: Bool
-    let code: Int
-    let message: String
-    let result: BatteryInfo
-}
-
 /*
  class DataFetcher: ObservableObject {
      @Published var data: String = ""
